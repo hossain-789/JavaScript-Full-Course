980ef5c07cf0dbcd2bc8fa998dab84e12eff3b9c
Hossain Ahmed <hossainahmed.7709@gmail.com>
2025-05-12 21:40:16 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,54 @@
+// 1. Declarin Variables
+let fullName = "Hossain Ahmed";
+const age = 25;
+var country = "Bangladesh";
+console.log (fullName, age, country);
+
+// 2. Changing Variable Values
+let score = 10;
+console.log("initial score:", score);
+score = 20;
+console.log("updated score:", score);
+
+// 3. String Concatenation
+let athorName = "Jhon";
+let secondName = "Smith";
+
+let = athorName + " " + secondName;
+console.log("Full Name:", athorName);
+
+
+// 4. Arrithmetic with Variables
+let num1 = 5;
+let num2 = 3;
+
+let sum = num1 + num2;
+let product = num1*num2;
+
+console.log("Sum:", sum);
+console.log("Product:", product);
+
+// 5. Using Template Literals
+let username = "Mark";
+let message = `Welcome, ${username}!`;
+
+console.log(message);
+
+// 6. Boolean Variables
+
+let isLoggedIn = true;
+let isAdmin = false;
+
+console.log("User logged in:", isLoggedIn);
+console.log("User is Admin:", isAdmin);
+
+// 7. Swapping Two Variables
+let a = 5;
+let b = 10;
+console.log("Before swap", a, b);
+    // Swapping
+let temp = a;
+a = b;
+b = temp;
+console.log("After swap:", a, b);
+
