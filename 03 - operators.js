becd03fd5a267aa297b2d8c634be559014d75dad
Hossain Ahmed <hossainahmed.7709@gmail.com>
2025-05-14 21:46:01 +0600

    updated operators

--- 03 - operators.js
+++ 03 - operators.js
@@ -40,11 +40,36 @@
 
 
 //While Loop with Increment
-let i = 0;
-while (i < 5){
-    console.log("value = ", i);
-    i++;
-}
+// let i = 0;
+// while (i < 5){
+//     console.log("value = ", i);
+//     i++;
+// }
+
+console.log("arithmetic operators");
+let a = 5, b = 8;
+console.log(a + " + " + b + " = " + (a+b)); // some as: 5 + 8 = 10;
+console.log(a + " + ", b + " = " + (a-b)); // some as: 5 - 8 = 0;
+console.log(a + " + " + b + " = " + (a*b)); // some as: 5 * 8 = 40;
+console.log(a + " + " + b + " = " + (a/b)); // some as: 5 / 8 = 0.625;
+console.log(a + " + " + b + " = " + (a%b)); // some as: 5 % 8 = 5;
+
+
+
+console.log("assignment opetators");
+let aso = 10; // 10;
+console.log("a = " + aso); // 10;
+aso += 5; // 10 + 5;
+console.log("a = " + aso); // 15;
+aso-= 5; // 15 - 5;
+console.log("a = " + aso); // 10;
+aso *= 5; // 10 * 5;
+console.log("a = " + aso); // 50;
+aso /= 5; // 50 / 5;
+console.log("a = " + aso); // 10;
+aso %= 5; // 10 % 5;
+console.log("a = " + aso) // 0;
+
 
 
 
