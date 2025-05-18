3450bd5d7afaeec9aeb5ccc891599f8442b39cb5
Hossain Ahmed <hossainahmed.7709@gmail.com>
2025-05-18 09:03:19 +0600

    operators

---
+++ 04 - condition.js
@@ -0,0 +1,35 @@
+// // Check if a number is positive or negative
+// let number = 0;
+
+// if (number > 0) {
+//   console.log("this Number is Positive");
+// } else if (number < 0) {
+//   console.log("The Number is Nagitive");
+// } else {
+//   console.log("The number Zero");
+// }
+
+
+// const youAge = 20;
+// const yourBrotherAge = 30;
+
+// if(youAge > yourBrotherAge){
+//   console.log("You are older");
+  
+// }else(youAge < yourBrotherAge){
+
+// }
+
+const isNumber = 30;
+const inNumber = 30;
+
+if (isNumber < inNumber ){
+  console.log("is number coto");
+  
+} else if(isNumber > inNumber){
+  console.log("in number boro");
+  
+} else{
+  console.log("same this number");
+  
+}
